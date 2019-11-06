8b99ca351da1830af0cebabb3a4f19b93a0476d5
guciy <guciy@qq.com>
2019-11-06 10:48:14 +0800

    halo2

--- halo.java
+++ halo.java
@@ -1,9 +1,9 @@
 public class halo{
 
     public static void main(String[] args) {
-        System.out.println("1");
+        System.out.println("3");
         System.out.println("halo");
-        System.out.println("1");
+        System.out.println("4");
         System.out.println("halo");
     }
 
